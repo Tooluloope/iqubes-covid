b06ae580d01fc1f64cd7675869078fa8e73057b2
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-07-19 19:05:36 +0100

    PRoject init with tailwind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App min-h-screen w-screen py-20">
+			<div className="container mx-auto">
+				<div className="flex  justify-between items-center mx-auto">
+					<div className="border border-gray-400 shadow rounded w-full lg:w-1/2 lg:mr-2">
+						<p className="text-left p-3 pb-0 font-bold">Covid overview</p>
+						<div className="flex justify-between items-start py-8 pb-10">
+							<div className="md:w-1/3 w-full">
+								<p className="text-red-600 text-2xl lg:text-3xl">36,107</p>
+								<p className="text-gray-600 font-semibold">Confirmed Cases</p>
+							</div>
+
+							<div className="md:w-1/3 w-full">
+								<p className="text-green-600 text-2xl lg:text-3xl">36,107</p>
+								<p className="text-gray-600 font-semibold">Recovered</p>
+							</div>
+
+							<div className="md:w-1/3 w-full">
+								<p className="text-gray-600 text-2xl lg:text-3xl">36,107</p>
+								<p className="text-gray-600 font-semibold">Deaths</p>
+							</div>
+						</div>
+					</div>
+					<div className=" flex justify-between items-start py-8 pb-10 w-full lg:w-1/2 lg:ml-2">
+						<div className="w-full md:w1/2 border border-gray-400 shadow rounded md:mr-2">
+							<p>Recovery rate</p>
+						</div>
+						<div className="w-full md:w1/2 border border-gray-400 shadow rounded md:ml-2">
+							<p>Fatality rate</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
